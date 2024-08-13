58ee217c34e482b1bd2f99295ebf2b4be1dbcee8
Brandon Chung <jbx.chung@gmail.com>
2024-08-13 15:54:10 -0400

    resize canvas based on window size

--- src/components/KonvaWrapper/KonvaWrapper.tsx
+++ src/components/KonvaWrapper/KonvaWrapper.tsx
@@ -1,11 +1,40 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
+import { Stage } from "react-konva";
 
-import './KonvaWrapper.scss';
+import "./KonvaWrapper.scss";
 
 const KonvaWrapper: FC = () => {
+  const wrapperDivRef = useRef<HTMLDivElement>(null);
+
+  const [canvasDimensions, setCanvasDimensions] = useState({
+    width: 0,
+    height: 0
+  })
+
+  const handleWrapperResize = () => {
+    if (wrapperDivRef.current) {
+      setCanvasDimensions({
+        width: wrapperDivRef.current.offsetWidth,
+        height: wrapperDivRef.current.offsetHeight,
+      });
+    }
+  };
+
+  useEffect(() => {
+    // initialize canvas dimensions
+    handleWrapperResize();
+    // register resize event listener and clean it up on destroy
+    window.addEventListener("resize", handleWrapperResize);
+    return () => window.removeEventListener("resize", handleWrapperResize);
+  }, []);
+
   return (
-    <div className="konva-wrapper">
-      placeholder
+    <div ref={wrapperDivRef} className="konva-wrapper">
+      <Stage
+        width={canvasDimensions.width}
+        height={canvasDimensions.height}
+      >
+      </Stage>
     </div>
   );
 }
